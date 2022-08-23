8e16eb59fcff55e9ece52a23943064c9468ce0be
raminrodbari <111448557+raminxz@users.noreply.github.com>
2022-08-23 15:47:44 +0430

    Create language.js

---
+++ js/language.js
@@ -0,0 +1,258 @@
+var dictionary = {
+    'site-name': {
+        'fa': 'وبسایت شخصی رامین رودباری',
+        'en': 'Ramin Rodbari &#8217;s Personal Website',
+    },
+    'fullname': {
+        'fa': 'رامین رودباری',
+        'en': 'Ramin Rodbari &#8217;s',
+    },
+    'description': {
+        'fa': 'وبسایت شخصی',
+        'en': 'Personal Website',
+    },
+    'job': {
+        'fa': 'طراح و توسعه دهنده وب',
+        'en': 'Web Designer And Developer',
+    },
+    'age': {
+        'fa': 'سن',
+        'en': 'Age',
+    },
+    'relation': {
+        'fa': 'وضعیت تاهل',
+        'en': 'Material Status',
+    },
+    'military': {
+        'fa': 'وضعیت سربازی',
+        'en': 'Military Service Situation',
+    },
+    'military-des': {
+        'fa': 'پایان خدمت',
+        'en': 'The End Of Military Service',
+    },
+    'relation-value': {
+        'fa': 'مجرد',
+        'en': 'Single',
+    },
+    'address': {
+        'fa': 'آدرس',
+        'en': 'Address',
+    },
+    'address-value': {
+        'fa': 'ایران - بندر عباس',
+        'en': 'Iran - Bandar Abbas',
+    },
+    'mobile': {
+        'fa': 'شماره تماس',
+        'en': 'Phone Number',
+    },
+    'mail': {
+        'fa': 'ایمیل',
+        'en': 'E-mail',
+    },
+    'founder': {
+        'fa': 'موسس',
+        'en': 'Founder Of',
+    },
+    'founder-value': {
+        'fa': 'نوتریکا',
+        'en': 'Notryca',
+    },
+    'edu': {
+        'fa': 'تحصیلات',
+        'en': 'Education',
+    },
+    'teach': {
+        'fa': 'تدریس',
+        'en': 'Teaching',
+    },
+    'exper': {
+        'fa': 'تجربه',
+        'en': 'Experience',
+    },
+    'exper-title': {
+        'fa': 'طراحی و توسعه وب سایت',
+        'en': 'Website Design And Development',
+    },
+    'front': {
+        'fa': 'فرانت اند : ',
+        'en': 'Front End : ',
+    },
+    'back': {
+        'fa': 'بک اند : ',
+        'en': 'Back End : ',
+    },
+    'web-course': {
+        'fa': 'دوره آموزش طراحی سایت',
+        'en': 'Website Design Training Course',
+    },
+    'pro-course': {
+        'fa': 'آموزش برنامه نویسی',
+        'en': 'Teaching Programming',
+    },
+    'uni': {
+        'fa': 'دانشگاه',
+        'en': 'University Of',
+    },
+    'ghias': {
+        'fa': 'غیاث الدین جمشید کاشانی',
+        'en': 'Ghiaseddin Jamshid Kashani',
+    },
+    'orien': {
+        'fa': 'گرایش',
+        'en': 'Orientation',
+    },
+    'soft': {
+        'fa': 'نرم افزار',
+        'en': 'Software',
+    },
+    'bachelor': {
+        'fa': 'کارشناسی مهندسی نرم افزار',
+        'en': 'Bachelor Of Software Engineering',
+    },
+    'associate': {
+        'fa': 'کاردانی مهندسی نرم افزار',
+        'en': 'Associate Degree Of Software Engineering',
+    },
+    'language': {
+        'fa': 'زبان انگلیسی',
+        'en': 'English Language',
+    },
+    'skills': {
+        'fa': 'مهارت ها',
+        'en': 'Skills',
+    },
+    'hour': {
+        'fa': 'ساعت',
+        'en': 'Hours',
+    },
+    'year': {
+        'fa': 'سال',
+        'en': 'Years',
+    },
+    'home': {
+        'fa': 'خانه',
+        'en': 'Home',
+    },
+    'biography': {
+        'fa': 'بیوگرافی',
+        'en': 'Biography',
+    },
+    'bio': {
+        'fa': 'بیو ',
+        'en': 'Bio',
+    },
+    'graphy': {
+        'fa': 'گرافی',
+        'en': 'graphy',
+    },
+    'biography-des': {
+        'fa': 'اطلاعات شخصی و رزومه کاری',
+        'en': 'Personal Information And Resume',
+    },
+    'portfolio': {
+        'fa': 'نمونه کار',
+        'en': 'Portfolio',
+    },
+    'port': {
+        'fa': 'نمونه ',
+        'en': 'Port',
+    },
+    'folio': {
+        'fa': 'کار',
+        'en': 'folio',
+    },
+    'portfolio-des': {
+        'fa': 'قبل از سفارش طراحی وبسایت نمونه کارهای من رو مشاهده کنین',
+        'en': 'See My Portfolio Before Ordering A Website Design',
+    },
+    'see': {
+        'fa': 'مشاهده وبسایت',
+        'en': 'See Website',
+    },
+    'atras': {
+        'fa': 'گروه نرم افزاری اترس',
+        'en': 'Atrassoft Group',
+    },
+    'farham': {
+        'fa': 'آی ای او سرویس توکن',
+        'en': 'Ieo Service Token',
+    },
+    'robo': {
+        'fa': 'ادور تایزر توکن',
+        'en': 'Advertizer Token',
+    },
+    'blog': {
+        'fa': 'وبلاگ',
+        'en': 'Blog',
+    },
+    'blog-1': {
+        'fa': 'وب',
+        'en': 'Bl',
+    },
+    'blog-2': {
+        'fa': 'لاگ',
+        'en': 'og',
+    },
+    'blog-des': {
+        'fa': 'مقالات و ویدیو های آموزشی، اخبار تکنولوژی',
+        'en': 'Educational Articles And Videos, Technology News',
+    },
+    'coming': {
+        'fa': 'به زودی',
+        'en': 'Coming Soon',
+    },
+    'programming': {
+        'fa': 'دوره آموزش برنامه نویسی',
+        'en': 'Programming Training Course',
+    },
+    'web-design': {
+        'fa': 'دوره آموزش طراحی سایت',
+        'en': 'Website Design Training Course',
+    },
+    'october': {
+        'fa': 'مهر',
+        'en': 'Oct',
+    },
+    'september': {
+        'fa': 'شهریور',
+        'en': 'Sep',
+    },
+    'contact': {
+        'fa': 'تماس',
+        'en': 'Contact',
+    },
+    'information': {
+        'fa': 'اطلاعات',
+        'en': 'Information',
+    },
+    'contact-des': {
+        'fa': 'اطلاعات تماس جهت مشاوره، همکاری، و سفارش پروژه',
+        'en': 'Contact Information For Consulting, Collaboration, And Project Ordering',
+    },
+    'send-message': {
+        'fa': 'پیام بفرست !',
+        'en': 'Send Message !',
+    },
+    'send-message2': {
+        'fa': 'ارسال پیام',
+        'en': 'Send Message',
+    }
+};
+var langs = ['fa', 'en'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function() {
+    current_lang_index = ++current_lang_index % 2;
+    current_lang = langs[current_lang_index];
+    translate();
+}
+
+function translate() {
+    $("[data-translate]").each(function() {
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
